0c0963449b3880985ef8c34923f68b656292fe39
John Richard Chipps-Harding <john.chipps-harding@phntms.com>
2021-03-27 17:24:33 +0000

    Update admin/src/containers/App/index.js
    Co-authored-by: Paulo <mfjpaulo@gmail.com>

--- admin/src/containers/App/index.js
+++ admin/src/containers/App/index.js
@@ -7,16 +7,14 @@ import { NotFound } from "strapi-helper-plugin";
 import pluginId from "../../pluginId";
 import HomePage from "../HomePage";
 
-const App = () => {
-  return (
-    <div>
-      <GlobalStyle />
-      <Switch>
-        <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <GlobalStyle />
+    <Switch>
+      <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
+      <Route component={NotFound} />
+    </Switch>
+  </div>
+);
 
 export default App;
