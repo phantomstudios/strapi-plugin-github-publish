32c376b5383869c051fca0ebdedd8189a56a0fd8
Valentin Day <valentin.day@colibris.xyz>
2022-03-27 20:27:04 +0200

    Migrate to strapi v4

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,53 +1,53 @@
+import { prefixPluginTranslations } from '@strapi/helper-plugin';
 import pluginPkg from "../../package.json";
-import App from "./containers/App";
-import Initializer from "./containers/Initializer";
-import lifecycles from "./lifecycles";
 import pluginId from "./pluginId";
-import trads from "./translations";
+import Initializer from "./components/Initializer";
+import PluginIcon from './components/PluginIcon';
 
-export default (strapi) => {
-  const pluginDescription =
-    pluginPkg.strapi.description || pluginPkg.description;
-  const icon = pluginPkg.strapi.icon;
-  const name = pluginPkg.strapi.name;
+const { name, displayName } = pluginPkg.strapi;
 
-  const plugin = {
-    blockerComponent: null,
-    blockerComponentProps: {},
-    description: pluginDescription,
-    icon,
-    id: pluginId,
-    initializer: Initializer,
-    injectedComponents: [],
-    isReady: false,
-    isRequired: pluginPkg.strapi.required || false,
-    layout: null,
-    lifecycles,
-    mainComponent: App,
-    name,
-    preventComponentRendering: false,
-    trads,
-    menu: {
-      pluginsSectionLinks: [
-        {
-          destination: `/plugins/${pluginId}`,
-          icon,
-          label: {
-            id: `${pluginId}.plugin.name`,
-            defaultMessage: name,
-          },
-          name,
-          permissions: [
-            // Uncomment to set the permissions of the plugin here
-            // {
-            //   action: '', // the action name should be plugins::plugin-name.actionType
-            //   subject: null,
-            // },
-          ],
-        },
-      ],
-    },
-  };
+export default {
+  register(app) {
+    app.addMenuLink({
+      to: `/plugins/${pluginId}`,
+      icon: PluginIcon,
+      intlLabel: {
+        id: `${pluginId}.plugin.name`,
+        defaultMessage: displayName,
+      },
+      Component: async () => {
+        const component = await import(/* webpackChunkName: "github-publish" */ './pages/HomePage');
 
-  return strapi.registerPlugin(plugin);
+        return component;
+      },
+    });
+    app.registerPlugin({
+      id: pluginId,
+      name,
+      initializer: Initializer,
+      isReady: false,
+    })
+  },
+
+  async registerTrads({ locales }) {
+   const importedTrads = await Promise.all(
+     locales.map(locale => {
+       return import(`./translations/${locale}.json`)
+         .then(({ default: data }) => {
+           return {
+             data: prefixPluginTranslations(data, pluginId),
+             locale,
+           };
+         })
+         .catch(() => {
+           return {
+             data: {},
+             locale,
+           };
+         });
+     })
+   );
+
+   return Promise.resolve(importedTrads);
+ },
 };
---
+++ server/controllers/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  'publish': require('./publish')
+};
